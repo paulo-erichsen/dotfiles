12e2452912dbac38ead4c4b5c8c51e48e7d4fb94
Paulo Fagundes <paulohefagundes@gmail.com>
2018-10-19 07:48:27 -0600

    add some minor improvements

--- .local/bin/install-packages.sh
+++ .local/bin/install-packages.sh
@@ -5,3 +5,7 @@ sudo pacman -S --needed - < ~/.pkglists/arch-official.txt
 # - it should:
 # if arch-linux, install arch-official, yay, aur packages through yay
 # if macos, install brew and the brew packages and brew cask packages
+# python: something like
+# python3 -m pip install --user flake8 mypy pylint numpy matplotlib black virtualenv virtualenvwrapper
+# see an example of different packages to be installed - https://github.com/hugdru/dotfiles
+#   such as ruby (gem), python (pip), javascript (npm), arch (pacman), etc...
