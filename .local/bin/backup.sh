e7077c8045aee29180cb2cb299e8a9407cfc166f
Paulo Fagundes <paulohefagundes@gmail.com>
2020-07-04 14:12:35 -0600

    systemd: cleanup old unused unit files and add sync-to-nas
    - also delete backup.sh since sync-to-nas.service replaces it

--- .local/bin/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/rsync -av --progress --delete /mnt/data/my_files/           nas:/share/my_files/
-/usr/bin/rsync -av --progress --delete /mnt/data/music/              nas:/share/Multimedia/music/
-/usr/bin/rsync -av --progress --delete /mnt/data/Games/Emulators/    nas:/share/games/Emulators/
